6520e817d63249342d045fa586ed36a8c2c1f704
bertalanV <bertalan.verebelyi@gmail.com>
2022-02-13 14:31:06 +0100

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,75 @@
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    // let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    // let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+
+struct Ipv4Addr {
+    // --snip--
+}
+
+struct Ipv6Addr {
+    // --snip--
+}
+
+enum IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
+
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
